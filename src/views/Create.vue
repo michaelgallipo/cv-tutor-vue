f3807bac1fd19d0b01cf887f6256833b8321ac7d
michael <michael.gallipo@gmail.com>
2020-03-22 21:14:17 -0400

    Finished Create Form incl subjects, added subjects to tutors index, made email mail:to link

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,129 +1,203 @@
 <template>
-	<div class="create">
-		<div class="container">
-			<h1>Create an Account to Become a Tutor</h1>
-			<ul>
-				<li class="text-danger" v-for="error in errors">{{ error }}</li>
-			</ul>
-			<div class="form-group">
-				<label>Name:</label>
-				<input type="text" class="form-control" v-model="name" />
-			</div>
-			<div class="form-group">
-				<label>Street:</label>
-				<input type="text" class="form-control" v-model="street" />
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-1 col-form-label">City:</label>
-				<div class="col-sm-3">
-					<input type="text" class="form-control" v-model="city" />
-				</div>
-				<label class="col-sm-1 col-form-label">State:</label>
-				<div class="col-sm-1">
-					<input type="text" class="form-control" v-model="state" />
-				</div>
-				<label class="col-sm-2 col-form-label">Zip Code:</label>
-				<div class="col-sm-2">
-					<input type="text" class="form-control" v-model="zip" />
-				</div>
-			</div>
-			<div class="form-group">
-				<label>School:</label>
-				<input type="text" class="form-control" v-model="school" />
-			</div>
-			<div class="form-group">
-				<label>Email:</label>
-				<input type="text" class="form-control" v-model="email" />
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-1 col-form-label">Phone:</label>
-				<div class="col-sm-3">
-					<input type="text" class="form-control" v-model="phone" />
-				</div>
-				<label class="col-sm-2 col-form-label">Phone Visible?:</label>
-				<div class="col-sm-3">
-					<div class="form-check form-check-inline">
-						<input
-							class="form-check-input"
-							type="radio"
-							name="inlineRadioOptions"
-							id="inlineRadio1"
-							value="true"
-							v-model="phone_visible"
-						/>
-						<label class="form-check-label" for="inlineRadio1">Yes</label>
-					</div>
-					<div class="form-check form-check-inline">
-						<input
-							class="form-check-input"
-							type="radio"
-							name="inlineRadioOptions"
-							id="inlineRadio2"
-							value="false"
-							v-model="phone_visible"
-						/>
-						<label class="form-check-label" for="inlineRadio2">No</label>
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<label>About Me:</label>
-				<textarea
-					type="textarea"
-					class="form-control"
-					rows="5"
-					v-model="about"
-				/>
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-2 col-form-label">Min Grade:</label>
-				<div class="col-sm-1">
-					<input type="text" class="form-control" v-model="grade_min" />
-				</div>
-				<label class="col-sm-2 col-form-label">Max Grade:</label>
-				<div class="col-sm-1">
-					<input type="text" class="form-control" v-model="grade_max" />
-				</div>
-				<label class="col-sm-1 col-form-label">Rate:</label>
-				<div class="col-sm-2">
-					<input type="text" class="form-control" v-model="rate" />
-				</div>
-			</div>
-			<div class="form-row">
-				<label class="col-form-label">Accept New Students: &emsp;</label>
-				<div class="form-check form-check-inline">
-					<input
-						class="form-check-input"
-						type="radio"
-						name="inlineRadioOptions"
-						id="inlineRadio1"
-						value="true"
-						v-model="accept_new"
-					/>
-					<label class="form-check-label" for="inlineRadio1">Yes</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input
-						class="form-check-input"
-						type="radio"
-						name="inlineRadioOptions"
-						id="inlineRadio2"
-						value="false"
-						v-model="accept_new"
-					/>
-					<label class="form-check-label" for="inlineRadio2">No</label>
-				</div>
-			</div>
-			<button class="btn btn-primary" v-on:click="submit()">Submit</button>
-		</div>
-	</div>
+  <div class="create">
+    <div class="container">
+      <h1>Create an Account to Become a Tutor</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>Name:</label>
+        <input type="text" class="form-control" v-model="name" />
+      </div>
+      <div class="form-group">
+        <label>Street:</label>
+        <input type="text" class="form-control" v-model="street" />
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-1 col-form-label">City:</label>
+        <div class="col-sm-3">
+          <input type="text" class="form-control" v-model="city" />
+        </div>
+        <label class="col-sm-1 col-form-label">State:</label>
+        <div class="col-sm-1">
+          <input type="text" class="form-control" v-model="state" />
+        </div>
+        <label class="col-sm-2 col-form-label">Zip Code:</label>
+        <div class="col-sm-2">
+          <input type="text" class="form-control" v-model="zip" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label>School:</label>
+        <input type="text" class="form-control" v-model="school" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="text" class="form-control" v-model="email" />
+      </div>
+      <div class="form-group row">
+        <div class="form-group col-sm-6">
+          <label>Password</label>
+          <input type="password" class="form-control" placeholder="Password" v-model="password" />
+        </div>
+        <div class="form-group col-sm-6">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Repeat Password"
+            v-model="password_confirmation"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-1 col-form-label">Phone:</label>
+        <div class="col-sm-3">
+          <input type="text" class="form-control" v-model="phone" />
+        </div>
+        <label class="col-sm-2 col-form-label">Phone Visible?:</label>
+        <div class="col-sm-3">
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio1"
+              value="true"
+              v-model="phone_visible"
+            />
+            <label class="form-check-label" for="inlineRadio1">Yes</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio2"
+              value="false"
+              v-model="phone_visible"
+            />
+            <label class="form-check-label" for="inlineRadio2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <label>About Me:</label>
+        <textarea type="textarea" class="form-control" rows="5" v-model="about" />
+      </div>
+      <div class="form-group">
+        <label>Subjects</label>
+        <br />
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox1"
+            value="true"
+            v-model="math"
+          />
+          <label class="form-check-label" for="checkbox1">Math &emsp;</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox2"
+            value="true"
+            v-model="reading"
+          />
+          <label class="form-check-label" for="checkbox2">Reading &emsp;</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox3"
+            value="true"
+            v-model="spelling"
+          />
+          <label class="form-check-label" for="checkbox2">Spelling &emsp;</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox4"
+            value="true"
+            v-model="special_needs"
+          />
+          <label class="form-check-label" for="checkbox4">Special Needs &emsp;</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox5"
+            value="true"
+            v-model="science"
+          />
+          <label class="form-check-label" for="checkbox5">Science &emsp;</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="checkbox6"
+            value="true"
+            v-model="social_studies"
+          />
+          <label class="form-check-label" for="checkbox4">Social Studies &emsp;</label>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Min Grade:</label>
+        <div class="col-sm-1">
+          <input type="text" class="form-control" v-model="grade_min" />
+        </div>
+        <label class="col-sm-2 col-form-label">Max Grade:</label>
+        <div class="col-sm-1">
+          <input type="text" class="form-control" v-model="grade_max" />
+        </div>
+        <label class="col-sm-1 col-form-label">Rate:</label>
+        <div class="col-sm-2">
+          <input type="text" class="form-control" v-model="rate" />
+        </div>
+      </div>
+      <div class="form-row">
+        <label class="col-form-label">Accept New Students: &emsp;</label>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id="inlineRadio1"
+            value="true"
+            v-model="accept_new"
+          />
+          <label class="form-check-label" for="inlineRadio1">Yes</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id="inlineRadio2"
+            value="false"
+            v-model="accept_new"
+          />
+          <label class="form-check-label" for="inlineRadio2">No</label>
+        </div>
+      </div>
+      <button class="btn btn-primary" v-on:click="submit()">Submit</button>
+    </div>
+  </div>
 </template>
 
 <script>
 var axios = require("axios");
 export default {
-	data: function() {
-		return {
+  data: function() {
+    return {
       name: "",
       street: "",
       city: "",
@@ -131,15 +205,23 @@ export default {
       zip: "",
       school: "",
       email: "",
+      password: "",
+      password_confirmation: "",
       phone: "",
       phone_visible: "",
       about: "",
       grade_min: "",
       grade_max: "",
       rate: "",
-      accept_new: "",
-			errors: []
-		};
+      accept_new: true,
+      math: false,
+      reading: false,
+      spelling: false,
+      special_needs: false,
+      science: false,
+      social_studies: false,
+      errors: []
+    };
   },
   methods: {
     submit: function() {
@@ -151,36 +233,42 @@ export default {
         zip: this.zip,
         school: this.school,
         email: this.email,
+        password: this.password,
+        password_confirmation: this.password_confirmation,
         phone: this.phone,
         phone_visible: this.phone_visible,
         about: this.about,
         grade_min: this.grade_min,
         grade_max: this.grade_max,
         rate: this.rate,
-        accept_new: this.accept_new
+        accept_new: this.accept_new,
+        math: this.math,
+        reading: this.reading,
+        spelling: this.spelling,
+        special_needs: this.special_needs,
+        science: this.science,
+        social_studies: this.social_studies
       };
-      console.log(params);
+      // console.log(params);
       axios
-				.post("/api/tutors", params)
-				.then(response => {
-					if (response.status === 200) {
+        .post("/api/tutors", params)
+        .then(response => {
+          if (response.status === 200) {
             window.alert("Account successfully created");
-            router.push('/');
-					}
+            router.push("/");
+          }
         })
-        .catch(
-          function(error) {
-            this.errors = error.response.data.errors;
-          }.bind(this)
-        );
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
     }
-   }
+  }
 };
 </script>
 
 <style>
 .create h1 {
-	text-align: center;
-	padding-bottom: 50px;
+  text-align: center;
+  padding-bottom: 50px;
 }
 </style>
--- src/views/Tutors.vue
+++ src/views/Tutors.vue
@@ -1,54 +1,69 @@
 <template>
-	<div class="tutors">
-		<div class="title">
-			<h1>See Our Tutors</h1>
-		</div>
-		<div v-for="tutor in tutors" class="col-lg-9">
-			<div class="card border-2" style="margin-bottom: 20px">
-				<div class="card-body">
-					<h4>{{ tutor.name }}</h4>
-					<p>
-						Location: {{ tutor.state }} &emsp; &emsp; School: {{ tutor.school }}
-					</p>
-					<p style="font-size:18px; margin-top:7px; font-style: italic">
-						About Me
-					</p>
-					<p>{{ tutor.about }}</p>
-					<p>Grade Levels: {{ tutor.grade_min }} - {{ tutor.grade_max }}</p>
-					<p>Rate: {{ tutor.rate }}</p>
-					<p>Contact: {{ tutor.email }}</p>
-					<p v-if="tutor.phone">Phone: {{ tutor.phone }}</p>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="tutors">
+    <div class="title">
+      <h1>See Our Tutors</h1>
+    </div>
+    <div v-for="tutor in tutors" class="col-lg-9">
+      <div class="card border-2" style="margin-bottom: 20px">
+        <div class="card-body">
+          <h4 style="font-weight:bold">{{ tutor.name }}</h4>
+          <p>Location: {{ tutor.state }} &emsp; &emsp; School: {{ tutor.school }}</p>
+          <p style="font-size:18px; margin-top:7px; font-style: italic">About Me</p>
+          <p>{{ tutor.about }}</p>
+          <label id="subjects">Subjects</label>
+          <ul class="list-group list-group-horizontal">
+            <div v-for="subject in tutor.subjects">
+              <li class="list-group-item border-0" id="subject">{{subject}}</li>
+            </div>
+          </ul>
+          <p>Grade Levels: {{ tutor.grade_min }} - {{ tutor.grade_max }}</p>
+          <p>Rate: {{ tutor.rate }}</p>
+          <p>
+            Contact:
+            <a v-bind:href="'mailto:' + tutor.email">{{ tutor.email }}</a>
+          </p>
+          <p v-if="tutor.phone_visible">Phone: {{ tutor.phone }}</p>
+          <p v-if="tutor.accept_new">Accepting New Students</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .title {
-	text-align: center;
-	padding-bottom: 20px;
+  text-align: center;
+  padding-bottom: 20px;
 }
 p {
-	margin-block-start: 1px;
-	margin-block-end: 1px;
+  margin-block-start: 1px;
+  margin-block-end: 1px;
+}
+
+#subjects {
+  font-weight: bold;
+  padding-top: 5px;
+}
+
+#subject {
+  padding: 0rem 1.25rem 0.4rem 0.4rem;
 }
 </style>
 
 <script>
 var axios = require("axios");
 export default {
-	data: function() {
-		return {
-			tutors: []
-		};
-	},
+  data: function() {
+    return {
+      tutors: []
+    };
+  },
 
-	created: function() {
-		axios.get("api/tutors").then(response => {
-			this.tutors = response.data;
-			console.log(this.tutors);
-		});
-	}
+  created: function() {
+    axios.get("api/tutors").then(response => {
+      this.tutors = response.data;
+      console.log(this.tutors);
+    });
+  }
 };
 </script>
